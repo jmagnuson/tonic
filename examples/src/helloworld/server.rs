14f24a6b7ff91a0f7d985df0d5114928fa5452a2
Jon Magnuson <jon.magnuson@gmail.com>
2022-02-25 10:09:55 -0600

    example: add SimpleLogger to server.rs
    Demonstrates how even though log's global max_level is set to Debug,
    Trace level logs are getting through for operations in h2. I suspect
    tracing's span logic may have a role here, but unsubstantiated.

--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -3,6 +3,28 @@ use tonic::{transport::Server, Request, Response, Status};
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
 
+struct SimpleLogger;
+
+static SIMPLE_LOGGER: SimpleLogger = SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        /*if !self.enabled(record.metadata()) {
+            return;
+        }*/
+
+        println!("{}:{} -- {}",
+                 record.level(),
+                 record.target(),
+                 record.args());
+    }
+    fn flush(&self) {}
+}
+
 pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
@@ -27,6 +49,12 @@ impl Greeter for MyGreeter {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    log::set_logger(&SIMPLE_LOGGER).unwrap();
+    log::debug!("this won't get logged");
+    log::set_max_level(log::LevelFilter::Debug);
+    log::debug!("this should get logged");
+
     let addr = "[::1]:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
